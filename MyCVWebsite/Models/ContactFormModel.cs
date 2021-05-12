381cbf32ad422f7b2443d10ada05519b4f40c8cf
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2021-05-12 14:12:13 +0200

    Made a contact form (doesn't send email)

---
+++ MyCVWebsite/Models/ContactFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCVWebsite.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        [MinLength(20, ErrorMessage = "Your message is too short. Try adding some context about why you're contacting me.")]
+        [StringLength(1000, ErrorMessage = "Your message is too long (max is 1000 characters)")]
+        public string Message { get; set; }
+    }
+}
